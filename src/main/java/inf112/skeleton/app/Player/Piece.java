557d399886abfd01d0e90c7eca54b3f2b13aeec5
EirikINF112 <eirik.sangolt+inf122@gmail.com>
2019-02-20 15:21:37 +0100

    Added debug movement with arrow keys

--- src/main/java/inf112/skeleton/app/Player/Piece.java
+++ src/main/java/inf112/skeleton/app/Player/Piece.java
@@ -3,10 +3,12 @@ package inf112.skeleton.app.Player;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class Piece extends Actor {
     private Sprite sprite;
+    private Vector2 position = new Vector2(0, 0);
 
     public Piece(Sprite sprite) {
         this.sprite = sprite;
@@ -19,9 +21,13 @@ public class Piece extends Actor {
 
     @Override
     protected void positionChanged() {
-        float x = getX();
-        float y = getY();
-        if(x > 11 || x < 0 || y > 11 || y < 0) throw new IllegalArgumentException();
-        this.sprite.translate(Gdx.graphics.getWidth()/12f*(0.25f+x), Gdx.graphics.getWidth()/12f*(0.25f+y));
+        Vector2 new_position = new Vector2(getX(), getY()); // Store it in a new variable in case the move is illegal
+        if(new_position.x > 11 || new_position.x < 0 || new_position.y > 11 || new_position.y < 0) throw new IllegalArgumentException();
+        this.sprite.setPosition(Gdx.graphics.getWidth()/12f*(0.25f+new_position.x), Gdx.graphics.getWidth()/12f*(0.25f+new_position.y));
+        position = new_position; // Movement complete, update stored position
+    }
+
+    public Vector2 getPosition() {
+        return position;
     }
 }
--- src/main/java/inf112/skeleton/app/RoboRally.java
+++ src/main/java/inf112/skeleton/app/RoboRally.java
@@ -2,45 +2,53 @@ package inf112.skeleton.app;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import inf112.skeleton.app.Player.Piece;
 
 public class RoboRally implements ApplicationListener {
     private SpriteBatch batch;
-    private BitmapFont font;
     private TextureRegion board;
     private Piece piece;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        font = new BitmapFont();
-        font.setColor(Color.RED);
+
         this.board = new TextureRegion(new Texture(Gdx.files.internal("textures/board1.png")), 0, 0, 1800, 1800);
         this.piece = new Piece(new Sprite(new Texture(Gdx.files.internal("textures/piece1.png")), 32, 32));
 
         // Sets position on the 12x12 grid
-        piece.setPosition(11, 11);
+        piece.setPosition(11, 10);
     }
-
-
 
     @Override
     public void dispose() {
         batch.dispose();
-        font.dispose();
     }
 
     @Override
     public void render() {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        Vector2 position = piece.getPosition();
+        try
+        {
+            if(Gdx.input.isKeyJustPressed(Input.Keys.DOWN)) piece.setPosition(position.x, position.y-1);
+            if(Gdx.input.isKeyJustPressed(Input.Keys.UP)) piece.setPosition(position.x, position.y+1);
+            if(Gdx.input.isKeyJustPressed(Input.Keys.LEFT)) piece.setPosition(position.x-1, position.y);
+            if(Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)) piece.setPosition(position.x+1, position.y);
+        }
+        catch(IllegalArgumentException e)
+        {
+            System.err.println("Tried to move out of bounds");
+        }
 
         batch.begin();
         batch.draw(board, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
