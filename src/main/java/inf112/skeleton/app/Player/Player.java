17c58a8ac8021305a31950b9c6063d556a0504fd
EirikINF112 <eirik.sangolt+inf122@gmail.com>
2019-03-20 14:40:21 +0100

    Added a series of small functions to the player class

--- src/main/java/inf112/skeleton/app/Player/Player.java
+++ src/main/java/inf112/skeleton/app/Player/Player.java
@@ -1,21 +1,56 @@
 package inf112.skeleton.app.Player;
 
 import com.badlogic.gdx.math.Vector2;
+import inf112.skeleton.app.Card.Card;
+
+import java.util.ArrayList;
 
 public class Player {
 
     private Vector2 position = new Vector2(0, 0);
 
-    private int hp;
+    private int hp = 3;
+    Piece piece;
+    ArrayList<Card> program;
 
-
-    public Vector2 getPosition () {
-
-        return position;
-
+    // Default constructor
+    public Player()
+    {
+        this("piece1/$dir.png");
     }
 
+    public Player(String spritePath)
+    {
+        piece = new Piece(spritePath);
+    }
 
+    // Sets hp to max
+    public void repair()
+    {
+        hp = 3;
+    }
 
+    // Does n damage and returns the remaining hp
+    public int hit(int damage)
+    {
+        hp -= damage;
+        if (hp < 0) hp = 0;
+        return hp;
+    }
 
+    public int getHp()
+    {
+        return hp;
+    }
+
+    public void setProgram(ArrayList<Card> program)
+    {
+        this.program = program;
+    }
+
+    // "Consumes" the first card of the program
+    public Card takeCard()
+    {
+        return program.remove(0);
+    }
 }
