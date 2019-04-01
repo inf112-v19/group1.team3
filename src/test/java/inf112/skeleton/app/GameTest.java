000fe35e2bbd0bccbbd2e32f26405eeceed29757
hev006 <amanda.s.helgesen@gmail.com>
2019-04-01 11:36:53 +0200

    Created GameTest.java

---
+++ src/test/java/inf112/skeleton/app/GameTest.java
@@ -0,0 +1,21 @@
+package inf112.skeleton.app;
+
+import com.badlogic.gdx.Input;
+import inf112.skeleton.app.Boards.Board.Board;
+import org.junit.Test;
+
+
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+
+    @Test
+    public void handleInputTest (){
+        Board board = new Board();
+        Game game = new Game(board, 1);
+
+
+
+    }
+}
