ac44690fd6590310739b7a8f32f0ff05d279233f
EirikINF112 <eirik.sangolt+inf122@gmail.com>
2019-04-30 14:31:45 +0200

    Re-added function that got lost during a commit and removed a broken test

--- src/test/java/inf112/skeleton/app/Player/PlayerTest.java
+++ src/test/java/inf112/skeleton/app/Player/PlayerTest.java
@@ -1,20 +1,7 @@
 package inf112.skeleton.app.Player;
 
-import inf112.skeleton.app.Boards.Board.Board;
-import inf112.skeleton.app.Main;
-
-
-import static org.junit.Assert.*;
-
 public class PlayerTest {
 
-
-    Player player = new Player();
-
-    //@Test
-    public void getPosition() {
-        System.out.println(player.piece.getPosition());
-    }
 
 }
 
