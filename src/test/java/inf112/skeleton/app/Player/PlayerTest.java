bbaf930dd0c1aae7efb0322beb6cd6854cf3706c
huvarz <havard.kyllingstad@gmail.com>
2019-05-03 16:50:45 +0200

    Added PlayerTest

--- src/test/java/inf112/skeleton/app/Player/PlayerTest.java
+++ src/test/java/inf112/skeleton/app/Player/PlayerTest.java
@@ -1,7 +1,23 @@
 package inf112.skeleton.app.Player;
 
+import com.badlogic.gdx.math.Vector2;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class PlayerTest {
 
+    Vector2 position = new Vector2(0, 0);
+    Player player = new Player(1, position);
 
+    // Check if player ends up at starting pos after respawn
+    @Test
+    public void reSpawn() {
+        player.piece.setPosition(new Vector2(10,10));
+        player.respawn();
+        Vector2 positionStart = new Vector2(0,0);
+        assertEquals(player.piece.getPosition(), positionStart);
+    }
 }
 
